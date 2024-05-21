fa59b7dd75f787727552ae9c5425bb58f94ed7e4
Kenneth Vargas <kennethvargas26@gmail.com>
2024-05-21 23:17:30 +0800

    add a image slider component

---
+++ resources/js/Components/ImageSlider.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+
+const props = defineProps(['images']);
+
+</script>
+<template>
+    <div class="slider-conatainer relative w-4/5">
+        <swiper
+            :slidesPerView="5"
+            :spaceBetween="80"
+            :loop="true"
+            :modules="modules">
+                <swiper-slide
+                    v-for="image of images"
+                    :key="image.id">
+                    <img :src="'storage/skills/'+image.filename" alt="">
+                </swiper-slide>
+        </swiper>
+    </div>
+</template>
