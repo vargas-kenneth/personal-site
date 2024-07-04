c76ec788f3aa27e4ee636eebc141617c65fa99d4
Kenneth Vargas <kennethvargas26@gmail.com>
2024-07-04 23:21:44 +0800

    add footer section

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer class="text-white bg-darker-purple h-72">
+        <div class="w-4/5 flex flex-1 h-4/5 flex-wrap mx-auto">
+            <div class="w-1/3">
+                <ul class="h-full flex justify-center items-center gap-5">
+                    <li><img class="w-5"
+                            src="storage/images/icons/social-facebook.png" 
+                            alt="facebook"></li>
+                    <li><img class="w-5"
+                        src="storage/images/icons/social-github.png" 
+                        alt="github"></li>
+                    <li><img class="w-5"
+                        src="storage/images/icons/social-linkedin.png" 
+                        alt="linkedin"></li>
+                </ul>
+            </div>
+            <div class="w-1/3 flex justify-center items-center">
+                <img class="w-16" src="storage/images/logo/logo.png" alt="Logo">
+            </div>
+            <div class="w-1/3 flex justify-center items-end flex-col ">
+                <a href="">vargaskenneth26@gmail.com</a>
+                <span>+63 945-454-579</span>
+            </div>
+        </div>
+        <div class="h-1/5 flex items-center justify-center">
+            <span class="block">Copyright &copy; 2024 Personal Portfolio</span>
+        </div>
+    </footer>
+</template>
