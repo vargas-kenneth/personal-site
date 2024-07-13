e54ba84f1d35cae0d08290bcf5310f6e1be81086
Kenneth Vargas <kennethvargas26@gmail.com>
2024-07-13 23:24:30 +0800

    add mobile responsive to intro section

--- resources/js/Components/Home/IntroSection.vue
+++ resources/js/Components/Home/IntroSection.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, computed } from 'vue';
 import GridBox from '@/Components/GridBox.vue';
 
 const title = 'A Brief Introduction';
@@ -6,23 +7,29 @@ const subTitle = 'Trusted Developer & Collaborator';
 const content = 'To be a web developer that provides an opportunity to enhance my skill and ability. And to apply my knowledge and creativity that will contribute in organization. A self-motivated web developer with ability to learn more in rapid changing environments and compositions.';
 
 const parentClass = 'w-1/4';
+
+const screenWidth = ref(window.innerWidth);
+
+const isMobile = computed(() => {
+    return screenWidth.value <= 1080;
+});
 </script>
 
 <template>
-    <div id="intro" class="h-96 w-full flex justify-center align-center bg-charcoal-gray text-white">
+    <div id="intro" class="h-96 sm:h-96 w-full sm:py-0 flex justify-center align-center bg-charcoal-gray text-white">
         <div class="w-4/5 flex">
-            <div class="w-full lg:w-3/4 my-auto px-6">
-                <div id="intro-header" class="mb-6">
-                    <span class="uppercase text-xl">{{ title }}</span>
+            <div class="w-full xl:w-3/4 my-auto px-2 sm:px-6">
+                <div id="intro-header" class="mb-3 sm:mb-6">
+                    <span class="uppercase text-md sm:text-xl">{{ title }}</span>
                 </div>
-                <div id="intro-title" class="mb-6">
-                    <h2 class="text-3xl">{{ subTitle }}</h2>
+                <div id="intro-title" class="mb-3 sm:mb-6">
+                    <h2 class="text-2xl sm:text-3xl">{{ subTitle }}</h2>
                 </div>
                 <div id="intro-content">
-                    <p class="text-lg">{{ content }}</p>
+                    <p class="text-md sm:text-lg">{{ content }}</p>
                 </div>
             </div>
-            <GridBox :parentClass="parentClass"></GridBox>
+            <GridBox v-if="!isMobile" :parentClass="parentClass"></GridBox>
         </div>
     </div>
 </template>
